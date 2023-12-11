c56e53cd9c68ef8c2bec749fa0ba09bdb0984c27
vohizy <99481011+Vohizy@users.noreply.github.com>
2023-12-11 18:36:52 +0300

    chore: ricecooker.py

--- ricecooker.py
+++ ricecooker.py
@@ -1,5 +1,6 @@
 import time
 
+
 class RiceCooker:
     def __init__(self):
         self.power = False
@@ -9,68 +10,69 @@ class RiceCooker:
 
     def turn_on(self):
         self.power = True
-        print("Le rice cooker est allumé.")
+        print("The rice cooker is turned on.")
 
     def turn_off(self):
         self.power = False
-        print("Le rice cooker est éteint.")
+        print("The rice cooker is turned off.")
 
     def choose_function(self, user_input):
         if user_input == "1":
             self.turn_on()
             try:
-                action = self.get_valid_number("Que voulez-vous faire ? (1 pour cuire / 2 pour bouillir): ", [1, 2])
+                action = self.get_valid_number("What do you want to do? (1 to cook / 2 to boil): ", [1, 2])
                 if action == 1:
-                    duration = self.get_valid_number("Combien de minutes souhaitez-vous cuire ? ")
-                    temperature = self.get_valid_number("À quelle température souhaitez-vous cuire ? ")
+                    duration = self.get_valid_number("How many minutes do you want to cook? ")
+                    temperature = self.get_valid_number("At what temperature do you want to cook? ")
                     self.cook(duration, temperature)
                 elif action == 2:
                     self.boil()
                 else:
-                    print("Action non reconnue.")
+                    print("Unrecognized action.")
             except ValueError as e:
-                print(f"Erreur: {e}")
+                print(f"Error: {e}")
         elif user_input == "2":
             self.turn_off()
         else:
-            print("Commande non reconnue.")
+            print("Unrecognized command.")
 
     def get_valid_number(self, prompt_message, valid_choices=None):
         while True:
             try:
                 user_input = input(prompt_message)
                 if not user_input.isdigit():
-                    raise ValueError("Veuillez saisir un nombre.")
+                    raise ValueError("Please enter a number.")
                 user_input = int(user_input)
 
                 if valid_choices is not None and user_input not in valid_choices:
-                    raise ValueError("Veuillez saisir un nombre valide parmi les choix spécifiés.")
-                    
+                    raise ValueError("Please enter a valid number among the specified choices.")
+
                 return user_input
             except ValueError as e:
-                print(f"Erreur: {e}")
+                print(f"Error: {e}")
+
     def cook(self, duration, temperature):
         try:
-            print(f"Cuisson en cours pendant {duration} minute(s) à {temperature}°C...")
+            print(f"Cooking in progress for {duration} minute(s) at {temperature}°C...")
             self.sleep(duration)
-            print("Cuisson terminée!")
+            print("Cooking completed!")
         except ValueError as e:
-            print(f"Erreur: {e}")
+            print(f"Error: {e}")
 
     def boil(self):
         try:
-            print("Bouillir de l'eau en cours...")
-            boiling_time = 10 # Durée de la simulation (10 minutes par exemple)
+            print("Boiling water in progress...")
+            boiling_time = 10  # Simulation duration (10 minutes, for example)
             current_temperature = 0
 
             for i in range(1, boiling_time + 1):
                 current_temperature += 10
                 self.sleep(1)
-                print(f"Température de l'eau: {current_temperature}°C - Durée restante: {boiling_time - i} minutes")
+                print(f"Water temperature: {current_temperature}°C - Remaining time: {boiling_time - i} minutes")
 
-            print("L'eau a atteint 100°C. Arrêt automatique.")
+            print("Water has reached 100°C. Automatic shutdown.")
         except ValueError as e:
-            print(f"Erreur: {e}")
+            print(f"Error: {e}")
 
     def sleep(self, minutes):
         milliseconds = minutes * 60 * 1000
@@ -78,11 +80,12 @@ class RiceCooker:
         while int(time.time() * 1000) < start_time + milliseconds:
             pass
 
-# Exemple d'utilisation
+
+# Example usage
 my_rice_cooker = RiceCooker()
 
 try:
-    user_decision = my_rice_cooker.get_valid_number("Salut !! Vous êtes sur le rice cooker. Voulez-vous l'allumer ? (1 pour oui / 2 pour non): ", [1, 2])
+    user_decision = my_rice_cooker.get_valid_number("Hello! You are using the rice cooker. Do you want to turn it on? (1 for yes / 2 for no): ", [1, 2])
     my_rice_cooker.choose_function(str(user_decision))
 except ValueError as e:
-    print(f"Erreur: {e}")
+    print(f"Error: {e}")
